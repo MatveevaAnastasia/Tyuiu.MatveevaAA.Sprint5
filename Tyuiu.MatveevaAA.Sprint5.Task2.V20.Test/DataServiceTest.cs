f0c1928155d2486e6ce50ed6a93ba23503fb8355
Anastasia Matveeva <kekkekyshk@gmail.com>
2025-11-23 03:42:15 +0500

    Task2

---
+++ Tyuiu.MatveevaAA.Sprint5.Task2.V20.Test/DataServiceTest.cs
@@ -0,0 +1,26 @@
+﻿using Tyuiu.MatveevaAA.Sprint5.Task2.V20.Lib;
+
+namespace Tyuiu.MatveevaAA.Sprint5.Task2.V20.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+
+
+            string direct = System.IO.Path.GetTempPath();
+
+
+
+            string path = Path.GetTempFileName();
+            string apath = Path.Combine(direct, path);
+
+            FileInfo fileInfo = new FileInfo(apath);
+            bool fileExist = fileInfo.Exists;
+            bool wait = true;
+            Assert.AreEqual(true, fileExist);
+        }
+    }
+}
---
+++ Tyuiu.MatveevaAA.Sprint5.Task2.V20/Program.cs
@@ -0,0 +1,36 @@
+﻿using Tyuiu.MatveevaAA.Sprint5.Task2.V20.Lib;
+
+namespace Tyuiu.MatveevaAA.Sprint5.Task2.V20
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+            Console.Title = "Спринт #5 | Выполнилa:  Матвеева Анастасия | ПКТБ-25-1 ";
+            Console.WriteLine("*******************************************************************************");
+            Console.WriteLine("* Спринт #5                                                                   *");
+            Console.WriteLine("* Тема: Базовые навыки работы в с#                                            *");
+            Console.WriteLine("* Задание #1                                                                  *");
+            Console.WriteLine("* Вариант #30                                                                 *");
+            Console.WriteLine("* Выполнилa:  Матвеева Анастасия | ПКТБ-25-1                                   *");
+            Console.WriteLine("*******************************************************************************");
+            Console.WriteLine("* Условие                                                                     *");
+            Console.WriteLine("* Дано выражение вычислить его значение при x = 3, результат сохранить        *");
+            Console.WriteLine("* в текстовый файл OutPutFileTask0.txt и вывести на консоль. Округлить до     *");
+            Console.WriteLine("* трёх знаков после запятой                                                   *");
+            Console.WriteLine("*******************************************************************************");
+            Console.WriteLine("* Исходные данные:                                                            *");
+            Console.WriteLine("*******************************************************************************");
+            int startValue, stopValue;
+            startValue = -5;
+            stopValue = 5;
+            Console.WriteLine("* Результат:                                                                  *");
+            Console.WriteLine("*******************************************************************************");
+            string res = ds.SaveToFileTextData(startValue, stopValue);
+            Console.WriteLine("Файл: " + res);
+            Console.WriteLine("Создан");
+            Console.ReadKey();
+        }
+    }
+}
