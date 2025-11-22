1f6fe233e163181e77adff033ba8c5d30458db20
Anastasia Matveeva <kekkekyshk@gmail.com>
2025-11-23 04:09:23 +0500

    Task5

--- Tyuiu.MatveevaAA.Sprint5.Task5.V7/Program.cs
+++ Tyuiu.MatveevaAA.Sprint5.Task5.V7/Program.cs
@@ -24,23 +24,25 @@ namespace Tyuiu.MatveevaAA.Sprint5.Task5.V7
             Console.WriteLine("* Выполнила:  Матвеева Анастасия | ПКТБ-25-1                              *");
             Console.WriteLine("***************************************************************************");
             Console.WriteLine("* УСЛОВИЕ:                                                                *");
-            Console.WriteLine("* Дан файл, в котором есть набор целых значений. Найти факториал первого  *");
-            Console.WriteLine("* целого числа в файле.                                                   *");
-            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("* Дан файл С:\\DataSprint5\\InPutDataFileTask5V7.txt (файл взять из       *");
+            Console.WriteLine("* архива согласно вашему варианту. Создать папку в ручную                 *");
+            Console.WriteLine("* С:\\DataSprint5\\ и скопировать в неё файл) в котором есть набор целых  *");
+            Console.WriteLine("* значений. Найти факториал первого целого числа в файле. Полученный      *");
+            Console.WriteLine("* результат вывести на консоль. У вещественных значений округлить до трёх *");
+            Console.WriteLine("* знаков после запятой.                                                   *");
             Console.WriteLine("***************************************************************************");
             Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
             Console.WriteLine("***************************************************************************");
-
             string path = @"C:\DataSprint5\InPutDataFileTask5V7.txt";
-            Console.WriteLine($"Данные находятся в файле: {path}");
+            Console.WriteLine("Файл: " + path);
 
             Console.WriteLine("***************************************************************************");
-            Console.WriteLine("* РЕЗУЛЬТАТ                                                               *");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
             Console.WriteLine("***************************************************************************");
-
-            double res = ds.LoadFromDataFile(path);
-            Console.WriteLine($"Факториал 1-го целого числа = {res}");
+            var result = ds.LoadFromDataFile(path);
+            Console.WriteLine(result);
             Console.ReadKey();
         }
     }
 }
+    
