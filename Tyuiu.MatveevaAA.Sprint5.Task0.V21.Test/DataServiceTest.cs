8ccc322aed50919c2952f47b6402a04fd577fb6b
Anastasia Matveeva <kekkekyshk@gmail.com>
2025-11-23 03:17:18 +0500

    Add project files.

---
+++ Tyuiu.MatveevaAA.Sprint5.Task0.V21.Test/DataServiceTest.cs
@@ -0,0 +1,20 @@
+﻿using System.Diagnostics;
+using Tyuiu.MatveevaAA.Sprint5.Task0.V21.Lib;
+
+namespace Tyuiu.MatveevaAA.Sprint5.Task0.V21.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidExpression()
+        {
+            string path = @"C:\Users\parse\source\repos\Tyuiu.MatveevaAA.Sprint5\Tyuiu.MatveevaAA.Sprint5.Task0.V21\bin\Debug\net8.0\OutPutFileTask0.txt";
+
+            FileInfo fileInfo = new FileInfo(path);
+            bool fileExist = fileInfo.Exists;
+            bool wait = true;
+            Assert.AreEqual(wait, fileExist);
+        }
+    }
+}
