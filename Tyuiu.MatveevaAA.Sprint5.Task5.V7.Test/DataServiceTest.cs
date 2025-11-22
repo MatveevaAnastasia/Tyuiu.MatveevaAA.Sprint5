7fa1e17ec0cc9b8350652f725edbc2c96a8a3ca9
Anastasia Matveeva <kekkekyshk@gmail.com>
2025-11-23 04:06:06 +0500

    Task5

---
+++ Tyuiu.MatveevaAA.Sprint5.Task5.V7.Test/DataServiceTest.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.IO;
+
+using Tyuiu.MatveevaAA.Sprint5.Task5.V7.Lib;
+
+namespace Tyuiu.MatveevaAA.Sprint5.Task5.V7.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidLoadFromDataFile()
+        {
+            DataService ds = new DataService();
+            string path = @"C:\DataSprint5\InPutDataFileTask5V7.txt";
+
+            var res = ds.LoadFromDataFile(path);
+            int wait = 40320;
+            Assert.AreEqual(wait, res);
+        }
+
+        [TestMethod]
+        public void CheckLoadFromDataFile()
+        {
+            string path = @"C:\DataSprint5\InPutDataFileTask5V7.txt";
+
+            FileInfo fileInfo = new FileInfo(path);
+            bool fileExists = fileInfo.Exists;
+            bool wait = true;
+            Assert.AreEqual(wait, fileExists);
+        }
+    }
+}
