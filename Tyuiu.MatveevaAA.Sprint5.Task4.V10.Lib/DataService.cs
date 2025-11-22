cef0b3ec03a8632771b50b72a6bc1bd1a391173d
Anastasia Matveeva <kekkekyshk@gmail.com>
2025-11-23 03:58:59 +0500

    Task4

---
+++ Tyuiu.MatveevaAA.Sprint5.Task4.V10.Lib/DataService.cs
@@ -0,0 +1,16 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint5;
+
+namespace Tyuiu.MatveevaAA.Sprint5.Task4.V10.Lib
+{
+    public class DataService : ISprint5Task4V10
+    {
+        public double LoadFromDataFile(string path)
+        {
+            string strx = File.ReadAllText(path);
+            strx = strx.Replace('.', ',');
+            double x = Convert.ToDouble(strx);
+            double res = Math.Round(Math.Pow(x, 3) * 1.2 * x + 2, 3);
+            return res;
+        }
+    }
+}
